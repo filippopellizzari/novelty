83f9183334988d186280e29695f42124aa7ecc2b
filippo <filippo.pellizzari109@gmail.com>
2018-02-21 19:55:38 +0100

    prod settings fix.

--- novelty/production_settings.py
+++ novelty/production_settings.py
@@ -1,7 +1,9 @@
 from .settings import *
+import dj_database_url
 
+ENVIRONMENT = 'production'
 DEBUG = False
-
+ALLOWED_HOSTS = ['']
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
 ]
@@ -12,3 +14,7 @@ WEBPACK_LOADER = {
             'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.prod.json'),
         }
 }
+
+DATABASES['default'] = dj_database_url.config(
+    default='postgres://gqlqkdnbagwckx:e84b56e5519326ae3626ea1a61e6fdf941ab2da8573610bc37ac54ad4686e8ec@ec2-54-235-64-195.compute-1.amazonaws.com:5432/d5ti7qeqk5nrnr'
+)
