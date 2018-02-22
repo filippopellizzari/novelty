85e6b80a117af3f5c5bb5d06cda4a0824487e6a5
filippo <filippo.pellizzari109@gmail.com>
2018-02-22 20:23:55 +0100

    db sett.

--- novelty/settings/base.py
+++ novelty/settings/base.py
@@ -98,6 +98,20 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'filippo',
+        'USER': 'filippo',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
--- novelty/settings/local.py
+++ novelty/settings/local.py
@@ -4,19 +4,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'filippo',
-        'USER': 'filippo',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
 
 WEBPACK_LOADER = {
     'DEFAULT': {
