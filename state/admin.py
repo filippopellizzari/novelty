09c95a9bd59453ba50d073a200a233043b08b804
filippo <filippo.pellizzari109@gmail.com>
2018-03-25 18:57:10 +0200

    Profile delete permissions.

--- state/admin.py
+++ state/admin.py
@@ -4,11 +4,13 @@ from .models import *
 class ProfileAdmin(admin.ModelAdmin):
     readonly_fields = ('email','page','questionNumber','valid_survey')
     list_display = ('email','page','questionNumber','valid_survey')
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 class AnswerAdmin(admin.ModelAdmin):
     readonly_fields = ('email','survey_id','question', 'answer')
     list_display = ('email','survey_id','question', 'answer',)
 
 
-admin.site.register(Profile, ProfileAdmin)
-admin.site.register(Answer, AnswerAdmin)
+#admin.site.register(Profile, ProfileAdmin)
+#admin.site.register(Answer, AnswerAdmin)
