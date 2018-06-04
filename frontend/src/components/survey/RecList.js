63c91ff05e19795c166436baf239c51008ba275f
filippo <filippo.pellizzari109@gmail.com>
2018-06-04 22:28:07 +0200

    Link tmdb from movie.

--- frontend/src/components/survey/RecList.js
+++ frontend/src/components/survey/RecList.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Slider from 'react-slick';
-import { Popup, Button } from 'semantic-ui-react'
-import {Row} from 'react-bootstrap';
-import axios from 'axios';
 
 import PosterSurvey from './PosterSurvey';
 import PrevArrow from './PrevArrow';
@@ -11,96 +8,45 @@ import NextArrow from './NextArrow';
 
 class RecList extends React.Component {
 
-    constructor(){
-      super();
-      this.state = {
-        trailer_key:"",
-        isTrailer:false,
-      };
-    }
+  handleSelect(movie){
+  console.log(movie.title);
+  }
 
+  render() {
+    var settings = {
+     dots: false,
+     infinite: false,
+     speed: 700,
+     slidesToShow: 5,
+     slidesToScroll: 4,
+     prevArrow: <PrevArrow/>,
+     nextArrow: <NextArrow/>
+    };
 
-    handleSelect(movie){
-      axios.get("http://api.themoviedb.org/3/movie/"+movie.id+"/videos?"+
-            "api_key=a070e12e1c6d7b84ebc1b172c841a8bf")
-        .then ((res) => {
-          if (res.data.results.length > 0){
-            this.setState({trailer_key:res.data.results[0].key, isTrailer:true})
-          }else{
-            this.setState({trailer_key:"", isTrailer:false})
-          }
-        });
-    }
-
-    render() {
-      var settings = {
-       dots: false,
-       infinite: false,
-       speed: 700,
-       slidesToShow: 5,
-       slidesToScroll: 4,
-       prevArrow: <PrevArrow/>,
-       nextArrow: <NextArrow/>
-      };
-
-      const style = {
-        borderRadius: 0,
-        opacity: 1,
-        padding: '2em',
-        backgroundColor:"#72797d",
-      }
-
-      const { isTrailer, trailer_key } = this.state
-
-      var trailerBotton = isTrailer
-      ? <Button href={"https://www.youtube.com/watch?v="+ trailer_key} target="_blank">
-          Watch Trailer
-        </Button>
-      : null
-
-      let movies = this.props.recs.map(
-        (movie) =>
+    let movies = this.props.recs.map(
+      (movie) =>
         <div key={movie.id}>
-          <Popup
-            key={movie.id}
-            trigger = {<PosterSurvey
-                        id={movie.id}
-                        title={movie.title}
-                        path={movie.poster_path}
-                        heigth="250"
-                        width="150"
-                        />}
-            position = 'left center'
-            inverted
-            onOpen = {this.handleSelect.bind(this,movie)}
-            style = {style}
-          >
-            <Popup.Header style={{textAlign:'center', marginTop:10}}>{movie.title}</Popup.Header>
-            <Popup.Content>
-              <div className="container">
-                <div style={{textAlign:'center', marginTop:10}}>
-                  {movie.release_date.substring(0,4)}
-                </div>
-                <div style={{textAlign:'center', marginTop:10}}>
-                  <Row>{movie.overview}</Row>
-                </div>
-                <div style={{textAlign:'center', marginTop:20}}>
-                  {trailerBotton}
-                </div>
-              </div>
-            </Popup.Content>
-          </Popup>
-        </div>
-      );
-      return (
-      <div>
-        <h3>List {this.props.name}</h3>
-        <Slider {...settings}>
-          {movies}
-        </Slider>
+          <a href={"https://www.themoviedb.org/movie/"+movie.id} target='_blank'>
+          <PosterSurvey
+            id={movie.id}
+            path={movie.poster_path}
+            heigth="250"
+            width="150"
+            onClick={this.handleSelect.bind(this)}
+          />
+          </a>
       </div>
-      );
-    }
+    );
+
+    return (
+    <div>
+      <h3>List {this.props.name}</h3>
+      <Slider {...settings}>
+        {movies}
+      </Slider>
+    </div>
+    );
+  }
 }
 
 export default RecList;
