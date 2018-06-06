07d5d0e8e2749c455b2a6df495ea6d91513dd262
filippo <filippo.pellizzari109@gmail.com>
2018-06-07 01:35:37 +0200

    Tmdb recsys backend complete.

--- recsys/services.py
+++ recsys/services.py
@@ -32,61 +32,133 @@ def top_rated(selected_items):
     response = exclude_seen(movies.results, selected_items)
     return response
 
-def top_pop(selected_items, genres_flag=False, crew_flag=False):
+def top_pop(selected_items, genres=False, crew=False, cast=False):
     tmdb.API_KEY = API_KEY
-    genres = []
+    genres_ids = []
     crew_ids = []
-
-    if(genres_flag):
+    cast_ids = []
+    if(genres):
         for movie_id in selected_items:
             movie = tmdb.Movies(movie_id).info()
             for genre in movie["genres"]:
-                genres.append(genre["id"])
+                genres_ids.append(genre["id"])
         #OR of genres
-        genres = '|'.join(str(x) for x in genres)
-        print("genres: " + genres)
-    if(crew_flag):
+        genres_ids = '|'.join(str(x) for x in genres_ids)
+        print("genres: " + genres_ids)
+    if(crew):
         for movie_id in selected_items:
             credits = tmdb.Movies(movie_id).credits()
-            for crew in credits["crew"][0:5]:
+            for crew in credits["crew"][0:4]:
                 crew_ids.append(crew["id"])
         #OR of crew
         crew_ids = '|'.join(str(x) for x in crew_ids)
         print("crew_ids: " + crew_ids)
+    if(cast):
+        for movie_id in selected_items:
+            credits = tmdb.Movies(movie_id).credits()
+            for cast in credits["cast"][0:4]:
+                cast_ids.append(cast["id"])
+        #OR of crew
+        cast_ids = '|'.join(str(x) for x in cast_ids)
+        print("cast_ids: " + cast_ids)
 
     discover = tmdb.Discover()
     response = discover.movie(
-        with_genres=[genres],
+        with_genres=[genres_ids],
         with_crew=[crew_ids],
+        with_cast=[cast_ids],
         sort_by='popularity.desc'
     )
     response = exclude_seen(discover.results, selected_items)
     return response
 
-def get_random(selected_items,reclist_length):
+def get_random(selected_items, reclist_length, genres=False, crew=False, cast=False):
     tmdb.API_KEY = API_KEY
 
-    latest_id = tmdb.Movies().latest()["id"]
-    random_ids = random.sample(range(1, latest_id), reclist_length*2)
+    genres_ids = []
+    crew_ids = []
+    cast_ids = []
+    if(genres):
+        for movie_id in selected_items:
+            movie = tmdb.Movies(movie_id).info()
+            for genre in movie["genres"]:
+                genres_ids.append(genre["id"])
+        #OR of genres
+        genres_ids = '|'.join(str(x) for x in genres_ids)
+        print("genres: " + genres_ids)
+    if(crew):
+        for movie_id in selected_items:
+            credits = tmdb.Movies(movie_id).credits()
+            for crew in credits["crew"][0:4]:
+                crew_ids.append(crew["id"])
+        #OR of crew
+        crew_ids = '|'.join(str(x) for x in crew_ids)
+        print("crew_ids: " + crew_ids)
+    if(cast):
+        for movie_id in selected_items:
+            credits = tmdb.Movies(movie_id).credits()
+            for cast in credits["cast"][0:4]:
+                cast_ids.append(cast["id"])
+        #OR of crew
+        cast_ids = '|'.join(str(x) for x in cast_ids)
+        print("cast_ids: " + cast_ids)
 
-    count = 0
+
+    discover = tmdb.Discover()
+    response = discover.movie(
+        with_genres=[genres_ids],
+        with_crew=[crew_ids],
+        with_cast=[cast_ids]
+    )
+
+    #this is due to a tmdb bug!!
+    if(discover.total_pages > 1000):
+        latest_page=1000
+    else:
+        latest_page=discover.total_pages
+    if(discover.total_results<reclist_length):
+        max_length=discover.total_results
+    else:
+        max_length=reclist_length
+
     movies = []
-    for movie_id in random_ids:
-        movie = get_movie_by_id(movie_id)
-        if(movie!=None):
-            movies.append(get_movie_by_id(movie_id))
+    count = 0
+    while(count < max_length):
+        random_page = random.randint(1,latest_page)
+        response = discover.movie(
+            with_genres=[genres_ids],
+            with_crew=[crew_ids],
+            with_cast=[cast_ids],
+            page=random_page
+        )
+        random_movie = discover.results[random.randint(0,len(discover.results)-1)]
+        already_selected=False
+        for movie in movies:
+            if(movie["id"]==random_movie["id"]):
+                already_selected=True
+        if(already_selected==False):
+            movies.append(random_movie)
             count = count + 1
-        if (count == reclist_length):
-            break
 
-    response = exclude_seen(movies, selected_items)
-    return response
 
+    movies = exclude_seen(movies, selected_items)
+
+    while(len(movies)<reclist_length):
+        random_movie = get_movie_by_id(random.randint(1,tmdb.Movies().latest()["id"]))
+        if(random_movie!=None):
+            already_selected=False
+            for movie in movies:
+                if(movie["id"]==random_movie["id"]):
+                    already_selected=True
+            if(already_selected==False):
+                movies.append(random_movie)
+        movies = exclude_seen(movies, selected_items)
+    return movies
 
 def recommend(selected_items, reclist_length):
-    movies = top_pop(selected_items, genres_flag=False, crew_flag=True)
+    movies = top_pop(selected_items, genres=True, crew=False, cast=True)
     #movies = top_rated(selected_items)
-    #movies = get_random(selected_items,reclist_length)
+    #movies = get_random(selected_items,reclist_length, genres=True, crew=False, cast=False)
 
     movies = movies[:reclist_length]
     return movies
