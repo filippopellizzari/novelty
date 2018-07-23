7c0d20c9eb66d47bafe9ce5f9aaad9d8c0f1ec13
filippo <filippo.pellizzari109@gmail.com>
2018-07-23 19:33:03 +0200

    Tmdb info on catalogue.

--- frontend/src/components/catalogue/PosterCatalogue.js
+++ frontend/src/components/catalogue/PosterCatalogue.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Image } from 'semantic-ui-react';
+import { Image,Button,Icon } from 'semantic-ui-react';
+
 
 class PosterCatalogue extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { imageStatus: "loading" };
-  }
-
   onClick() {
     this.props.onClick();
-  }
-
-  handleImageLoaded() {
-    this.setState({ imageStatus: "loaded" });
   }
 
   render() {
@@ -30,28 +22,30 @@ class PosterCatalogue extends React.Component {
   var cursor = isSelected
     ? ""
     : "pointer";
-/*
-  var imageSrc = this.props.path === null
-    ? "https://www.movieinsider.com/images/none_175px.jpg"
-    : "https://image.tmdb.org/t/p/w500" + this.props.path;
-
-*/
 
 var imageSrc = this.props.path === null
   ? null
   : "https://image.tmdb.org/t/p/w500" + this.props.path;
 
     return(
-      <Image
-        key={this.props.id}
-        src={imageSrc}
-        alt={this.props.title}
-        onClick={this.onClick.bind(this)}
-        style={{opacity:opacity,cursor:cursor}}
-        onLoad={this.handleImageLoaded.bind(this)}
-        bordered
-        rounded
-      />
+      <div>
+        <Image
+          key={this.props.id}
+          src={imageSrc}
+          alt={this.props.title}
+          onClick={this.onClick.bind(this)}
+          style={{opacity:opacity,cursor:cursor}}
+          bordered
+          rounded
+          />
+        <Button
+          style={{position: 'absolute', top: 0, left: 15, color: "#343a40"}}
+          href={"https://www.themoviedb.org/movie/"+this.props.id} target='_blank'
+          icon
+          size="mini"
+          ><Icon name='info circle'/>
+        </Button>
+    </div>
     );
   }
 }
